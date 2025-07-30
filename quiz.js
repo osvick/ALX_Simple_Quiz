894336008d833357f5670b699566527cd9837db4
Osinachi Aluche <osinachialuchevictor@gmail.com>
2025-07-30 18:12:10 +0100

    Corrected the feedback textcontent

--- quiz.js
+++ quiz.js
@@ -17,7 +17,7 @@ function checkAnswer() {
         feedback.textContent = "correct! well done.";
         feedback.style.color = "#28a745"; // green
     } else {
-        feedback.textContent = "That's incorrect. Try again!";
+        feedback.textContent = "Correct! Well done.";
         feedback.style.color = "#dc3545"; // red
     }
 }
